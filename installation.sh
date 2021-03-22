ee5af5921d539d60e6590cf6d6e68c5d7e5000b2
basu-dev <adbasudev54@gmail.com>
2021-03-22 18:28:43 +0545

    some changes to i3blocks and installation script

--- installation.sh
+++ installation.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-#sudo pacman -Syu
+sudo pacman -Syu --noconfirm
+
+for app in i3 i3blocks i3-gaps rofi picom ttf-font-awesome light lxappearance nitrogen python-pip acpi
+do sudo pacman -S $app --noconfirm
+done
+
 function install (){
 	echo "Install $1 ?";
 	select yn in "Yes" "No"; do
 		case $yn in 
-			Yes ) sudo pacman -S $2;break;;
+			Yes ) sudo pacman -S $2 --noconfirm;break;;
 			No ) break;
 		esac
 	done	
 }
-for app in i3blocks i3-gaps brave code thunar rofi nitrogen picom ttf-font-awesome arc-gtk-theme sardi-icons light python-pip lxappearance
+#run light command without sudo privillage
+sudo chmod +s /usr/bin/light
+
+for app in  brave code thunar arc-gtk-theme sardi-icons 
 
 do install $app $app
 done
@@ -20,4 +28,13 @@ echo "Install Powerline Shell for colorful shell?"
 		 No ) break;
 	esac
 done
+
+echo "Install Eye Dropper for picking color?"
+ select yn in "Yes" "No";do
+	 case $yn in
+		 Yes ) yay -S grabc --noconfirm;break;;
+		 No ) break;
+	esac
+done
+
 
